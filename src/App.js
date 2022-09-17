0c6e1c3c599101583946a7c13a81f6a180ad66ad
Vladimir Grigorev <ihahahoj@mail.ru>
2022-09-17 09:24:10 +0300

    feat: Сделан Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header> {/*  className="App-header">        
+        <div className="Row-markup">
+          <h1 className="Page-title">Наша продукция</h1>
+          <div className="Summary-order"> 
+            <p>3 товара</p>
+            <p>На сумму 3500р</p>
+          </div>
+          <img src={order} className="Order-icon" alt="order" />
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,7 +26,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>  
+        */}
+        <Header orderItems="3" orderPrice="3600" />
       </header>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import icon from "./Images/Header/icon.svg";
+import React from 'react'
+
+
+export default function Header(props) {
+    return (
+        <div className="Row-markup">
+            <div>
+                <h1 className="Page-title">Наша продукция</h1>
+            </div>
+            <div className="Right-block-markup">
+                <div className="Summary-order"> 
+                    <p>{props.orderItems} товара</p>
+                    <p>На сумму: {props.orderPrice} р.</p>
+                </div>
+                <img src={icon} className="Order-icon" alt="order" />
+            </div>
+            {/* <img src={logo} className="App-logo" alt="logo" />*/}
+        </div> 
+    )
+}
+
+
