0671cb7c4b743bb0373d10d1d75fec9237694b1b
Vladimir Grigorev <ihahahoj@mail.ru>
2022-09-24 07:00:08 +0300

    fix: добавлен вызов первых работ при старте и закоментирован вызов последней

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import App2 from './App2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App2 />
+    {/* Домашние задания до 23.09.2022 */}
+    <App />
+    {/* Домашнее задание от 23.09.2022
+    <App2 /> */}
   </React.StrictMode>
 );
