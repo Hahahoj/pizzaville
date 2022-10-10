b25a7e26de71ad394a05f0eab17b016bf8c4a270
Vladimir Grigorev <ihahahoj@mail.ru>
2022-10-10 11:26:56 +0300

    fix: определился с способом обновления формы регистрации

--- src/components/FormAuth.jsx
+++ src/components/FormAuth.jsx
@@ -1,4 +1,5 @@
 import "./FormAuth.css"
+import React, { useState } from 'react';
 import SwitchMode from "./auth/SwitchMode";
 import WindowsTitle from "./auth/WindowsTitle";
 import InputField from "./auth/InputField";
@@ -6,8 +7,39 @@ import CheckboxField from "./auth/CheckboxField";
 import ErrorField from "./auth/ErrorField";
 import SubmitButton from "./auth/SubmitButton"
 
+let ErrorFieldDesc;
+
+const UseState0 = () => {
+    ErrorFieldDesc = "";
+}
+
+const UseState1 = () => {
+    ErrorFieldDesc = "Логин или пароль неверен";
+}
+
+
+
+
 export default function FormAuth() {
 
+    const [value, setValue] = useState(0);
+
+    const useForceUpdate = () => { // integer state
+            setValue( value => value + 1 ); 
+            global.State=!global.State;
+            console.log("GS=",global.State);
+            if (global.State) { UseState0(); }
+                else { UseState1() };
+            console.log("ErrorFieldDesc=",ErrorFieldDesc);
+            console.log("value=",value);
+    }
+    
+
+    const SubmitUser = () => {
+        global.State=!global.State;
+        const forceUpdate = useForceUpdate();
+    }
+        
     return (
         <div className="FormAuth">
             <SwitchMode name="Зарегистрироваться" />
@@ -17,8 +49,8 @@ export default function FormAuth() {
             <InputField name="Пароль" type="password" description="Поле не должно быть пустым" />
             <CheckboxField description="Я согласен получать обновления на почту" /> 
             {/* <ErrorField description="" /> */}
-            <ErrorField description="Логин или пароль неверен" />
-            <SubmitButton name="Войти" />
+            <ErrorField description={ErrorFieldDesc} />
+            <SubmitButton name="Войти" onpress={useForceUpdate} />
         </div>
     );
 }
--- src/components/auth/SubmitButton.jsx
+++ src/components/auth/SubmitButton.jsx
@@ -1,9 +1,9 @@
 import "./SubmitButton.css"
 
-export default function SubmitButton({name}) {
+export default function SubmitButton({name,onpress}) {
     return (
         <div className="SubmitButton-markup">
-            <button className="SubmitButton">{name}</button>
+            <button className="SubmitButton" onClick={onpress}>{name}</button>
         </div>
     );
 }
