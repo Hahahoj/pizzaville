6fb28c298eec1d72a2c8bc73d9abf410f672f62c
Vladimir Grigorev <ihahahoj@mail.ru>
2022-12-02 08:25:49 +0300

    fix: быстрофикс странички отдельного товара

--- src/Pages/Order.jsx
+++ src/Pages/Order.jsx
@@ -5,11 +5,9 @@ export default function Order() {
     const pageID = useParams ()
 
     return (
-        <div className="ItemPage">
-            <div className="container3">
+        <div className="OrderPage">
                 <p>Заказ {pageID} принят на обработку</p>
                 <button>Продолжить</button>
-            </div> 
         </div>
     );
 }
